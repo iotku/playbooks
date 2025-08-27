fc7e64149c2185a6c8f2dee5fcb93285abb1aa28
Jonathan A. <me@iotku.pw>
2025-08-27 14:07:42 -0700

    chore: make fetch.sh executable

--- nixos/fetch.sh
+++ nixos/fetch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-rsync -av --progress /etc/nixos "./$1" --exclude 'wireguard.nix'
+    rsync -av --progress /etc/nixos/ "./$1" --exclude 'wireguard.nix' --exclude 'hardware-configuration.nix'
